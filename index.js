a7349012bfe2a959feba23b1ce6d760705228e00
Vinaykumar96 <vinayboss9669@gmail.com>
2025-07-30 13:25:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:"http://localhost:3000",
+    origin:process.env.FRONTEND_HOS,
     credentials:true
 };
 app.use(cors(corsOption)); 
